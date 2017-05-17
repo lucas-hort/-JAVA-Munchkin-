1ecf6b45ca37201210c11e61a66f4634b47b02e9
Unknown <lucashort7@gmail.com>
2017-05-17 15:43:23 -0300

    Att Monster

--- src/Munchkin/Monster.java
+++ src/Munchkin/Monster.java
@@ -5,5 +5,12 @@ package Munchkin;
  * @author Lucas Hort
  */
 public abstract class Monster {
+    private int level;
+    private String name;
+    BadStuff bd;
     
+    
+    void setBadStuff(BadStuff bd){
+        this.bd = bd;
+    }
 }
