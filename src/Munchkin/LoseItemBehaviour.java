3c9bc45e2d5b23b02f81cbaaa18d8fb57759c219
Unknown <lucashort7@gmail.com>
2017-05-17 15:48:39 -0300

    Strategy Pattern
    Behaviours adicionados

---
+++ src/Munchkin/LoseItemBehaviour.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Munchkin;
+
+/**
+ *
+ * @author Lucas Hort
+ */
+public class LoseItemBehaviour implements BadStuff{
+
+    @Override
+    public void badStuff() {
+        
+    }
+    
+}
